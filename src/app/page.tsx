9c0f9c7cea5cf1312236a20c24e11f8b0a64e208
ar-pixeltech <ar.pixeltech@gmail.com>
2025-06-26 17:24:19 +0530

    Initial commit integrating builder io

---
+++ src/app/page.tsx
@@ -0,0 +1,38 @@
+// Example file structure, app/[...page]/page.tsx
+// You could alternatively use src/app/[...page]/page.tsx
+import { builder } from "@builder.io/sdk";
+import { RenderBuilderContent } from "../components/builder";
+
+const PUBLIC_API_KEY = "358fb1baf0df4a24838c490ec295504f"
+
+// Replace with your Public API Key
+builder.init(PUBLIC_API_KEY);
+
+interface PageProps {
+  params: {
+    page: string[];
+  };
+}
+
+export default async function Page(props: PageProps) {
+  const model = "page";
+  const content = await builder
+    // Get the page content from Builder with the specified options
+    .get("page", {
+      userAttributes: {
+        // Use the page path specified in the URL to fetch the content
+        urlPath: "/" + (props?.params?.page?.join("/") || ""),
+      },
+      // Set prerender to false to return JSON instead of HTML
+      prerender: false,
+    })
+    // Convert the result to a promise
+    .toPromise();
+
+  return (
+    <>
+      {/* Render the Builder page */}
+      <RenderBuilderContent content={content} model={model} />
+    </>
+  );
+}
